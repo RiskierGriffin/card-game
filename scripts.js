75c9ff6ea2c6b68e7101a83cf9f6d08ec46daea9
RiskierGriffin <tenenbaumallan@gmail.com>
2018-02-14 18:40:49 -0500

    idek

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 function initialize(){
-    
+    function Card(value,suit,imgSrc){
+        this.value = value;
+        this.suit = suit;
+        this.imgSrc = imgSrc;
+    }
+    card = new Card(2,"clubs","images/cards/2c.png");
+    alert(card.value);
 }
